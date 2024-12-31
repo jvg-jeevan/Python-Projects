01eebe7659d0ed3871433bb8a3b6a7b7495fb16b
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2024-12-31 19:38:46 +0530

    Add files via upload

---
+++ Compare-Files/compare_files.py
@@ -0,0 +1,31 @@
+import hashlib
+from difflib import SequenceMatcher 
+
+def hash_file(fileName1, fileName2): 
+    # Use hashlib to store the hash of a file 
+    h1 = hashlib.sha1() 
+    h2 = hashlib.sha1() 
+
+    with open(fileName1, "rb") as file: 
+        # Read file in small chunks to handle large files
+        chunk = 0
+        while chunk:
+            chunk = file.read(1024)
+            h1.update(chunk)
+            
+    with open(fileName2, "rb") as file: 
+        # Read file in small chunks to handle large files
+        chunk = 0
+        while chunk:
+            chunk = file.read(1024)
+            h2.update(chunk)
+
+    # Return the SHA-1 hash of both files
+    return h1.hexdigest(), h2.hexdigest() 
+
+msg1, msg2 = hash_file("pd1.pdf", "pd1.pdf") 
+
+if msg1 != msg2: 
+    print("These files are not identical") 
+else: 
+    print("These files are identical")
